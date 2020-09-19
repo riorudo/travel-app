80eb30effc2e45a211fcb096a456bb35c6f68966
Michael Hochwieser <michael.hochwieser@gmail.com>
2020-09-19 15:30:29 +0200

    Create autocomplete list for destination search.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,15 +1,22 @@
+let chosenCity = null;
+
 function openFormDialog(event) {
     event.target.reportValidity();
     event.preventDefault();
     event.stopPropagation();
+    chosenCity = null;
     let formModal = document.getElementById('formModal');
+    let autoCompleteList = document.getElementById('autoCompleteList');
     Client.removeClass(formModal, 'display-none');
+    Client.addClass(autoCompleteList, 'display-none');
+    autocomplete(document.getElementById('destination'));
 }
 
 function closeFormDialog(event) {
     event.preventDefault();
     event.stopPropagation();
     let formModal = document.getElementById('formModal');
+    resetForm();
     Client.addClass(formModal, 'display-none');
 }
 
@@ -21,6 +28,126 @@ function submitForm(event) {
         elemTravelForm.reportValidity();
         return;
     }
+    const formData = getFormData();
+    restCall('book-travel', 'Post', formData)
+        .then(data => console.log(data));
+
+}
+
+function getFormData() {
+    const formData = {};
+    formData.firstName = document.getElementById('firstName').value.trim();
+    formData.lastName = document.getElementById('lastName').value.trim();
+    formData.destination = document.getElementById('destination').value.trim();
+    formData.destinationDetails = chosenCity;
+    formData.date = document.getElementById('date').value
+    return formData;
+}
+
+function resetForm() {
+    document.getElementById('firstName').value = null;
+    document.getElementById('lastName').value = null;
+    document.getElementById('destination').value = null;
+    document.getElementById('date').value = null;
+    resetAutocompleteList();
+    resetChosenCity();
+}
+
+function autocomplete(inputElem) {
+    inputElem.addEventListener("input", autocompleteHandler);
+}
+
+function autocompleteHandler() {
+    let val = document.getElementById('destination').value;
+    if (!val) {
+        resetAutocompleteList();
+        resetChosenCity();
+        return false;
+    }
+    restCall('cities', 'Get', {searchValue: val})
+        .then(data => renderAutocompleteList(data));
+}
+
+function renderAutocompleteList(data) {
+    resetAutocompleteList();
+    if (!data) {
+        return;
+    }
+    const autoCompleteList = document.getElementById('autoCompleteList');
+    let elemAutoCompleteItem;
+    data.forEach(item => {
+        elemAutoCompleteItem = document.createElement("DIV");
+        elemAutoCompleteItem.setAttribute("id", `${item['countryId']}`);
+        elemAutoCompleteItem.setAttribute("class", "autocomplete-item");
+        elemAutoCompleteItem.innerHTML = `${item['toponymName']} (${item['countryName']})`;
+        elemAutoCompleteItem.addEventListener("click", function (e) {
+            /*insert the value for the autocomplete text field:*/
+            document.getElementById('destination').value = e.target.innerHTML;
+            setChosenCity(item);
+            resetAutocompleteList();
+            Client.addClass(autoCompleteList, 'display-none');
+        });
+        document.getElementById('autoCompleteList').appendChild(elemAutoCompleteItem);
+    })
+    Client.removeClass(autoCompleteList, 'display-none');
+}
+
+function setChosenCity(city) {
+    chosenCity = city;
+}
+
+function resetChosenCity() {
+    chosenCity = null;
+}
+
+function resetAutocompleteList() {
+    document.getElementById('autoCompleteList').innerHTML = '';
+}
+
+
+async function restCall(url, method, data) {
+    const urlPrefix = 'http://localhost:3000/';
+    try {
+        let response;
+        if (method === 'Post') {
+            response = await fetch(urlPrefix + url, {
+                method: method,
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            });
+        } else if (method === 'Get') {
+            response = await fetch(urlPrefix + url + chainGetQueryParams(data), {
+                method: method,
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+        }
+        const resData = response.json();
+        return resData;
+    } catch (e) {
+        console.error(e);
+    }
+
+}
+
+function chainGetQueryParams(data) {
+    let queryParam = '';
+    if (Object.keys(data) && Object.keys(data).length > 0) {
+        queryParam = '?';
+        Object.keys(data).forEach((item, index) => {
+            if (index === 0) {
+                queryParam += `${item}=${data[item]}`;
+            } else {
+                queryParam += `&${item}=${data[item]}`;
+            }
+        });
+    }
+    return queryParam;
 }
 
 export {openFormDialog};
--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,9 @@
+const env = require('dotenv').config();
 const path = require('path')
 const express = require('express')
-const axios = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const cors = require('cors');
+const axios = require("axios");
 const app = express()
 
 app.use(express.static('../../dist'))
@@ -14,12 +15,49 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// Global variables
+let projectData = {
+    countryCodes: [],
+    recentEntries: []
+};
+const countyCodeAPIUrl = 'https://restcountries.eu/rest/v2/all';
+
+// Helper functions
+const getCountryCodes = async () => {
+    try {
+        const res = await axios.get(countyCodeAPIUrl);
+        const data = res.data.map((item) => {
+            return {name: item.name, alpha2Code: item.alpha2Code.toLowerCase()};
+        });
+        return data;
+    } catch (e) {
+        console.log("error", e);
+    }
+};
+
+const getCities = async (city) => {
+    const citiesAPIUrl = `http://api.geonames.org/searchJSON?q=${city}&maxRows=7&username=${process.env.GEONAMES_TOKEN}`;
+    try {
+        const res = await axios.get(citiesAPIUrl);
+        console.log(res.data.geonames);
+        return res.data.geonames;
+    } catch (e) {
+        console.log("error", e);
+    }
+}
+
 // designates what port the app will listen to for incoming requests
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
 
+app.get('/cities', async function (req, res) {
+    const cities = await getCities(req.query['searchValue']);
+    res.send(cities);
+})
+
 app.post('/book-travel', async function (req, res) {
+    console.log(req.body);
     res.send(mockAPIResponse);
 })
 
